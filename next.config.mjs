72b82534a5316cf30976e35222180888b3796cff
glovebx <glovebx@glovebxdeMacBook-Pro-2.local>
2025-09-08 16:10:09 +0800

    update config json files

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ import { get } from '@vercel/edge-config'
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-!process.env.SKIP_ENV_VALIDATION && (await import('./env.mjs'))
+// !process.env.SKIP_ENV_VALIDATION && (await import('./env.mjs'))
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
